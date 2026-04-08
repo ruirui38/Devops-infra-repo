41c32ef7996227563c70989b1bd5531bef47e5c9
ruirui38 <t.o.g.kendo0308@docomo.ne.jp>
2026-04-08 23:09:59 +0900

    modify module

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -17,3 +17,29 @@ provider "aws" {
 module "remote" {
   source = "../../modules/remote"
 }
+
+locals {
+  env = "prod"
+
+  project_name = "devops-${local.env}"
+
+  vpc_cidr = "10.0.0.0/21"
+
+  availability_zone = ["ap-northeast-1a","ap-northeast-1c"]
+
+  db_name = "tododb"
+
+  db_user = "admin"
+}
+
+module "base" {
+  source = "../../modules/base"
+
+  project_name = local.project_name
+
+  vpc_cidr = local.vpc_cidr
+
+  availability_zone = local.availability_zone
+
+  db_name = local.db_name
+}
---
+++ environments/prod/variables.tf
@@ -0,0 +1,7 @@
+variable "db_password" {
+  description = "DBのパスワード"
+
+  type = string
+
+  sensitive = true
+}
